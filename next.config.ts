8cdbfd11c69707d9d63de46b178a067633803077
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2025-12-03 13:59:06 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',       // включаем статический экспорт
+  distDir: 'out',         // папка, в которой Netlify будет искать статические файлы
+  experimental: {
+    appDir: true
+  }
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
